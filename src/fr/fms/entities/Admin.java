7b1a2eb7035860969780b8542829580769619167
CarolineDaumas <caroline.daumas@fms-ea.com>
2022-03-30 15:17:14 +0200

    Ajout des classes USER/CUSTOMER/ADMIN

--- src/fr/fms/entities/Admin.java
+++ src/fr/fms/entities/Admin.java
@@ -1,5 +1,41 @@
 package fr.fms.entities;
 
+import java.util.ArrayList;
+
 public class Admin extends User{
+	
+	private ArrayList<Customer> custs =new ArrayList<>();
+
+	
+
+	public Admin(int id, String firstname, String lastname, String email, String password, boolean role,
+			ArrayList<Customer> custs) {
+		super(id, firstname, lastname, email, password, role);
+		this.custs = custs;
+	}
+
+
+	public ArrayList<Customer> getCusts() {
+		return custs;
+	}
+
+	public void setCusts(ArrayList<Customer> custs) {
+		this.custs = custs;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString()+ "Admin [custs=" + custs + "]";
+	}
+	
+	public void createCustomer() {
+		
+		
+	}
+	
+	public void createAccount() {
+		
+		
+	}
 
 }
--- src/fr/fms/entities/Customer.java
+++ src/fr/fms/entities/Customer.java
@@ -1,5 +1,58 @@
 package fr.fms.entities;
 
+import java.util.ArrayList;
+
 public class Customer extends User{
 
+	private CurrentAccount cAccount;
+	private SavingsAccount sAccount;
+	private ArrayList<Banking> ops= new ArrayList<>();
+	
+	
+	public Customer(int id, String firstname, String lastname, String email, String password, boolean role,
+			CurrentAccount cAccount, SavingsAccount sAccount, ArrayList<Banking> ops) {
+		super(id, firstname, lastname, email, password, role);
+		this.cAccount = cAccount;
+		this.sAccount = sAccount;
+		this.ops = ops;
+	}
+
+
+	public CurrentAccount getcAccount() {
+		return cAccount;
+	}
+
+
+	public void setcAccount(CurrentAccount cAccount) {
+		this.cAccount = cAccount;
+	}
+
+
+	public SavingsAccount getsAccount() {
+		return sAccount;
+	}
+
+
+	public void setsAccount(SavingsAccount sAccount) {
+		this.sAccount = sAccount;
+	}
+
+
+	public ArrayList<Banking> getOps() {
+		return ops;
+	}
+
+
+	public void setOps(ArrayList<Banking> ops) {
+		this.ops = ops;
+	}
+
+
+	@Override
+	public String toString() {
+		return super.toString()+ "Customer [cAccount=" + cAccount + ", sAccount=" + sAccount + ", ops=" + ops + "]";
+	}
+	
+	
+	
 }
